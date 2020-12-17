e22383759c3d09d792dc470b92e6656ce11e70b5
Antonio Gabriel Fernandes <antgabrielsf@gmail.com>
2020-12-16 22:07:04 -0300

    Link se mexe

--- Proteja o arroz/Link.gd
+++ Proteja o arroz/Link.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 
+export var MAX_SPEED = 120
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity = Vector2.ZERO
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * MAX_SPEED
+	
+	velocity = move_and_slide(velocity)
